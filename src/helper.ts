ee02ed51d1472e6e8aa9edb33be885f72101e74c
RukshanDias <rukshandias002@gmail.com>
2024-03-05 00:10:54 +0530

    minor fix

--- src/helper.ts
+++ src/helper.ts
@@ -9,10 +9,8 @@ export class Helper {
 
         try {
             fs.accessSync(uri, fs.constants.F_OK);
-            console.log(`${uri} exists`);
             return true;
         } catch (err) {
-            console.error(`${uri} does not exist`);
             return false;
         }
     }
