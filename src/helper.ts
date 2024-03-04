7eb0c78635966b0eac275c57b0f740167e030829
RukshanDias <rukshandias002@gmail.com>
2024-03-04 21:43:46 +0530

    helpers to handle files in coverage generation

--- src/helper.ts
+++ src/helper.ts
@@ -1,5 +1,6 @@
 import path from "path";
 import * as vscode from "vscode";
+import * as fs from "fs";
 import { workspacePath } from "./fileMethodSelector";
 
 export class Helper {
@@ -16,7 +17,7 @@ export class Helper {
         }
     }
 
-    static convertPathToUnix(path: string) {
+    static convertPathToUnix(path: string): string {
         return path.replace(/\\+/g, "/");
     }
 
@@ -25,9 +26,47 @@ export class Helper {
         return relativePaths;
     }
 
-    static async executeCommandInTerminal(command: string): Promise<void> {
-        const terminal: vscode.Terminal = vscode.window.createTerminal("My Terminal");
-        terminal.sendText(command);
-        terminal.show();
+    static async generateCoverageReport(command: string, coverageFilePath: string): Promise<void> {
+        try {
+            await this.deleteFile(coverageFilePath);
+            console.log(`Deleted ${coverageFilePath}. Running coverage command...`);
+            await this.executeCommandInTerminal(command);
+            console.log("Coverage command completed.");
+            const regeneratedFile = await this.watchFile(coverageFilePath);
+            console.log(`${regeneratedFile} has been regenerated. Completed.`);
+        } catch (err) {
+            console.error("Error:", err);
+        }
+    }
+    static executeCommandInTerminal(command: string): Promise<void> {
+        return new Promise((resolve) => {
+            const terminal: vscode.Terminal = vscode.window.createTerminal("My Terminal");
+            terminal.sendText(command);
+            terminal.show();
+            resolve();
+        });
+    }
+
+    static watchFile(filePath: string): Promise<string> {
+        return new Promise((resolve) => {
+            const intervalId = setInterval(() => {
+                if (fs.existsSync(filePath)) {
+                    clearInterval(intervalId);
+                    resolve(filePath);
+                }
+            }, 2000);
+        });
+    }
+
+    static deleteFile(filePath: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            fs.unlink(filePath, (err) => {
+                if (err && err.code !== "ENOENT") {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        });
     }
 }
