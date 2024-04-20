1040033186d551f5fe2e4edace5493449bf349bf
RukshanDias <rukshandias002@gmail.com>
2024-04-20 14:05:46 +0530

    feature- clear highlights in editor

---
+++ src/codeLensProvider.ts
@@ -0,0 +1,75 @@
+import * as vscode from "vscode";
+import { FileMethodSelector } from "./fileMethodSelector";
+
+enum commandType {
+    Clear,
+}
+
+const clearCommand: vscode.Command = {
+    title: "Clear",
+    command: "jest-coverage.codelens.clear",
+    tooltip: "Tooltip provided by sample extension",
+};
+
+function setupCommand(codeLensObj: vscode.CodeLens, command: commandType): vscode.CodeLens {
+    let x: any;
+    switch (command) {
+        case commandType.Clear:
+            x = clearCommand;
+            break;
+    }
+    codeLensObj.command = x;
+    return codeLensObj;
+}
+
+/**
+ * CodelensProvider Class
+ */
+export class CodelensProvider implements vscode.CodeLensProvider {
+    private codeLenses: vscode.CodeLens[] = [];
+    public _onDidChangeCodeLenses: vscode.EventEmitter<void> = new vscode.EventEmitter<void>();
+    private FMSelector: FileMethodSelector;
+    private isVisible: boolean = false;
+
+    constructor(fileMethodSelector: FileMethodSelector) {
+        this.FMSelector = fileMethodSelector;
+
+        vscode.workspace.onDidChangeConfiguration((_) => {
+            this._onDidChangeCodeLenses.fire();
+        });
+    }
+
+    public provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
+        console.log("hiii");
+        this.codeLenses = [];
+        if (this.isCorrectDocument(document) && this.isVisible) {
+            console.log("in");
+            let x = this.FMSelector.getSelectionRange();
+            let line = x?.start !== undefined ? x.start : 0;
+            const range = document.lineAt(line - 1).range;
+
+            // CodeLens clear command
+            let codeLens1 = new vscode.CodeLens(range);
+            setupCommand(codeLens1, commandType.Clear);
+            this.codeLenses.push(codeLens1);
+        }
+        return this.codeLenses;
+    }
+
+    public setCodeLenses(codeLenses: vscode.CodeLens[]): void {
+        this.codeLenses = codeLenses;
+    }
+
+    public setVisibility(visible: boolean): void {
+        this.isVisible = visible;
+    }
+
+    private isCorrectDocument(received: vscode.TextDocument): boolean {
+        const files: string | undefined = this.FMSelector.getSelectionRange()?.filePath;
+        const type: string | undefined = this.FMSelector.getType();
+        if (files && type) {
+            return files === received.fileName && type === "CodeSelection";
+        }
+        return false;
+    }
+}
